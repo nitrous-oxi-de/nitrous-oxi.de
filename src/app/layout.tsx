82fcae0644f02984fc3d86aaf94b7df29720f43d
David <175678329+dvhsh@users.noreply.github.com>
2024-08-01 10:59:34 -0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import './globals.css'
 
-import type { Metadata } from 'next'
-import      { Inter    } from 'next/font/google'
+import { NextUIProvider } from "@nextui-org/react";
+
+import type { Metadata }  from 'next'
+import      { Inter    }  from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'nitrous-oxi.de',
-  description: 'open source OSINT tool with an exposed RESTful API and web search panel written for research purposes',
+  description: 'Open-source OSINT investigation suite with an exposed RESTful API written for research purposes.',
 }
 
 export default function RootLayout({
@@ -17,7 +19,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <NextUIProvider>
+        <head>
+        <meta charSet="UTF-8" />
+          <meta name="subject" content="Open-source OSINT Investigation Suite" />
+          <meta name="keywords" content="OSINT, free, open-source, lookup, search, nitrous, nitrousoxide, nitrous-oxide, email, username, phone, domain, ip" />
+          <meta name="author" content="Haruhime Holdings" />
+          <meta name="copyright" content="Haruhime Holdings LLC" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="robots" content="index, follow" />
+          <meta name="revised" content="Thursday, Aug 1st, 2024, 10:50 am" />
+          <meta property="og:title" content="OSINT - Nitrous Oxide" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://nitrous-oxi.de/" />
+          <meta property="og:site_name" content="Nitrous Oxide" />
+          <meta property="og:description" content="Nitrous Oxide is an open-source OSINT investigation suite with an exposed RESTful API written for research purposes." />
+          <meta name="twitter:title" content="OSINT - Nitrous Oxide" />
+          <meta name="twitter:description" content="Nitrous Oxide is an open-source OSINT investigation suite with an exposed RESTful API written for research purposes." />
+          <meta name="twitter:image" content="/path/to/your-image.jpg" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+          <meta name="format-detection" content="telephone=no" />
+        </head>
+        <body className={inter.className}>{children}</body>
+      </NextUIProvider>
     </html>
   )
 }
